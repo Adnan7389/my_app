2ba30662b3236683d11d22c2feb49ca7818280e0
Adnan7389! <152042446+Adnan7389@users.noreply.github.com>
2025-09-14 22:50:16 +0300

    feat(auth): add createUser and getUserByUsername database queries

--- db/queries.js
+++ db/queries.js
@@ -23,8 +23,34 @@ async function deleteAllUsernames() {
   await pool.query("DELETE FROM usernames");
 }
 
+// ---------functions for authentication ---------
+async function findUserByUsername(username) {
+  const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+  return rows[0];
+}
+
+async function findUserById(id) {
+  const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+  return rows[0];
+}
+
+async function createUser(username, hashedPassword) {
+  const { rows } = await pool.query(
+    "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *",
+    [username, hashedPassword]
+  );
+  return rows[0]; // return the new user
+}
+
+
 module.exports = {
   getAllUsernames,
   insertUsername,
   deleteAllUsernames,
+  // new
+  findUserByUsername,
+  findUserById,
+  createUser,
 };
